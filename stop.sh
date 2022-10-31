d17f7c5b14178948f5b28b0e9e23c9485bb91c72
Zero <a0940100@gmail.com>
2022-10-31 09:41:55 +0800

    修正stop

--- stop.sh
+++ stop.sh
@@ -4,6 +4,6 @@
 
 BOT_NAME=z-bot
 
-docker exec -it BOT_NAME /bin/bash -c "pkill -9 -P $(pgrep run.sh) 2> /dev/null"
-docker exec -it BOT_NAME /bin/bash -c "pkill -9 java"
-docker exec -it BOT_NAME /bin/bash -c "pkill -9 node"
+docker exec -it $BOT_NAME /bin/bash -c "pkill -9 -P $(pgrep run.sh) 2> /dev/null"
+docker exec -it $BOT_NAME /bin/bash -c "pkill -9 java"
+docker exec -it $BOT_NAME /bin/bash -c "pkill -9 node"
