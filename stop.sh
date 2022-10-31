6f89bd0db18c6705f2e41fdc5c2efcbc76120b74
Zero <a0940100@gmail.com>
2022-10-31 14:01:20 +0800

    調整stop.sh

--- stop.sh
+++ stop.sh
@@ -4,6 +4,4 @@
 
 export $(cat .env)
 
-docker exec -it $BOT_NAME /bin/bash -c "pkill -9 -P $(pgrep run.sh) 2> /dev/null"
-docker exec -it $BOT_NAME /bin/bash -c "pkill -9 java"
-docker exec -it $BOT_NAME /bin/bash -c "pkill -9 node"
+./runenv.sh
