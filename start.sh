6dd1d952d8e935ac627577211db17fca0b05d8c7
Zero <a0940100@gmail.com>
2022-11-01 17:28:04 +0800

    調整更新code的方式, 避免衝突

--- start.sh
+++ start.sh
@@ -7,7 +7,11 @@ export $(cat .env)
 SKIP_BUILD=$1
 
 # 更新code到最新
-docker exec $BOT_NAME /bin/bash -c "GIT_SSH_COMMAND=\"ssh -o StrictHostKeyChecking=no\" git pull origin HEAD"
+docker exec $BOT_NAME /bin/bash -c "
+GIT_SSH_COMMAND=\"ssh -o StrictHostKeyChecking=no\";
+git fetch --all;
+git reset --hard origin/master;
+git pull origin master;"
 
 # 先載module, 避免下載花太長時間
 docker exec $BOT_NAME /bin/bash -c "npm i"
