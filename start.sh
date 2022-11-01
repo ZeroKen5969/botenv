4dae9a24096cf3de26dd4123c278ed618b4de4ab
Zero <a0940100@gmail.com>
2022-11-01 15:35:46 +0800

    調整啟動執行順序

--- start.sh
+++ start.sh
@@ -9,12 +9,14 @@ SKIP_BUILD=$1
 # 更新code到最新
 docker exec -it $BOT_NAME /bin/bash -c "GIT_SSH_COMMAND=\"ssh -o StrictHostKeyChecking=no\" git pull origin HEAD"
 
-# 先停止機器人, 不然記憶體不夠使用...
+# 先載module, 避免下載花太長時間
+docker exec -it $BOT_NAME /bin/bash -c "npm i"
+
+# build前先停止機器人, 不然記憶體不夠使用...
 ./hotstop.sh
 
 # build code
 if [ "$SKIP_BUILD" != true ]; then
-    docker exec -it $BOT_NAME /bin/bash -c "npm i"
     docker exec -it $BOT_NAME /bin/bash -c "npm run build"
 fi
 docker exec -it $BOT_NAME /bin/bash -c "sed -i -e 's/\r$//' run.sh"
