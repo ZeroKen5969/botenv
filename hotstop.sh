3f33aef94eb0ae7ed4d8777ff8f21e794cfca881
Zero <a0940100@gmail.com>
2022-11-01 17:10:32 +0800

    調整指令

--- hotstop.sh
+++ hotstop.sh
@@ -4,6 +4,6 @@
 
 export $(cat .env)
 
-docker exec -it $BOT_NAME /bin/bash -c "pkill -9 -P $(pgrep run.sh) 2> /dev/null"
-docker exec -it $BOT_NAME /bin/bash -c "pkill -9 java"
-docker exec -it $BOT_NAME /bin/bash -c "pkill -9 node"
+docker exec $BOT_NAME /bin/bash -c "pkill -9 -P $(pgrep run.sh) 2> /dev/null"
+docker exec $BOT_NAME /bin/bash -c "pkill -9 java"
+docker exec $BOT_NAME /bin/bash -c "pkill -9 node"
--- start.sh
+++ start.sh
@@ -7,20 +7,20 @@ export $(cat .env)
 SKIP_BUILD=$1
 
 # 更新code到最新
-docker exec -it $BOT_NAME /bin/bash -c "GIT_SSH_COMMAND=\"ssh -o StrictHostKeyChecking=no\" git pull origin HEAD"
+docker exec $BOT_NAME /bin/bash -c "GIT_SSH_COMMAND=\"ssh -o StrictHostKeyChecking=no\" git pull origin HEAD"
 
 # 先載module, 避免下載花太長時間
-docker exec -it $BOT_NAME /bin/bash -c "npm i"
+docker exec $BOT_NAME /bin/bash -c "npm i"
 
 # build前先停止機器人, 不然記憶體不夠使用...
 ./hotstop.sh
 
 # build code
 if [ "$SKIP_BUILD" != true ]; then
-    docker exec -it $BOT_NAME /bin/bash -c "npm run build"
+    docker exec $BOT_NAME /bin/bash -c "npm run build"
 fi
-docker exec -it $BOT_NAME /bin/bash -c "sed -i -e 's/\r$//' run.sh"
-docker exec -it $BOT_NAME /bin/bash -c "chmod +x *.sh"
+docker exec $BOT_NAME /bin/bash -c "sed -i -e 's/\r$//' run.sh"
+docker exec $BOT_NAME /bin/bash -c "chmod +x *.sh"
 
 # 啟動服務
 docker exec -d $BOT_NAME /bin/bash -c "./run.sh > /proc/1/fd/1"
