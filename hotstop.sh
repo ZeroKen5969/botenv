6b154977f6721b8fa564952447c806263b62a236
ZeroKen5969 <37766281+ZeroKen5969@users.noreply.github.com>
2025-03-10 02:01:41 +0800

    Update hotstop.sh

--- hotstop.sh
+++ hotstop.sh
@@ -7,3 +7,4 @@ export $(cat .env)
 docker exec $BOT_NAME /bin/bash -c "pkill -9 -P $(pgrep run.sh) 2> /dev/null"
 docker exec $BOT_NAME /bin/bash -c "pkill -9 java"
 docker exec $BOT_NAME /bin/bash -c "pkill -9 node"
+docker exec $BOT_NAME /bin/bash -c "pkill -9 pkserver"
